3a607a993436c8c4974818d30aa5afed6124fe06
Luca Greco <luca.greco@alcacoop.it>
2012-10-29 19:02:27 +0000

    Deploy

---
+++ js/app/appcache_updates.js
@@ -0,0 +1,1 @@
+define(["require"],function(e){window.addEventListener("load",function(e){window.applicationCache&&window.applicationCache.addEventListener("updateready",function(e){window.applicationCache.status==window.applicationCache.UPDATEREADY&&(window.applicationCache.swapCache(),console.log("LOAD A NEW VERSION"),window.location.reload())},!1)},!1)})
--- js/app/version.js
+++ js/app/version.js
@@ -1,1 +1,1 @@
-define(["require"],function(e){return{app_version:"0.1.0"}})
+define(["require"],function(e){return{app_version:"0.1.1"}})
